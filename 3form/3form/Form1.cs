4f27f8c75041d217bd475506f0a8b5b419c7bb13
KinOnik <84587417+KinOnik@users.noreply.github.com>
2021-11-30 15:24:49 +0300

    Add files via upload

--- 3form/3form/Form1.cs
+++ 3form/3form/Form1.cs
@@ -19,7 +19,7 @@ namespace _3form
 
         private void button1_Click(object sender, EventArgs e)
         {
-
+            Result.Text = "";
             bool violationOfEquality = false;
             try
             {
